08193a02cbef41cd14d82ec721e1c5723be2e2b7
huqi <daqi0004@gmail.com>
2022-12-03 23:29:11 +0800

    完善配置文件功能

--- odf.py
+++ odf.py
@@ -1,3 +1,5 @@
+import codecs
+import json
 import os
 import sys
 import time
@@ -6,39 +8,41 @@ from docx import Document
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.oxml.ns import qn
 from docx.shared import Inches, Pt
-
-from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtGui import QGuiApplication, QIcon, QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QWidget
 from win32com import client as wc
+
+from odf_conf import *
 from odf_ui import Ui_Form
 
-MAIN_FRAME_SIZE = {'WIDTH': 300, 'HEIGHT': 400}
 
-    
 def get_file_extension(file):
     return os.path.splitext(file)[-1]
+
 
 def is_word_file(file):
     return any(file.endswith(ext) for ext in ['.doc', '.docx', 'DOC', '.DOCX'])
 
+
 def doc_to_docx(file):
     word = wc.Dispatch("Word.Application")
-    doc = word.Documents.Open(file) 
+    doc = word.Documents.Open(file)
     doc.SaveAs("{}x".format(new_file(file)), 12)
-    doc.Close() #关闭原来word文件
+    doc.Close()  # 关闭原来word文件
     word.Quit()
-    
+
+
 def get_time_str():
     return time.strftime("%Y%m%d%H%M%S", time.localtime())
-    
+
+
 def new_file(oldfile):
     path, file = os.path.split(oldfile)
     filename, ext = os.path.splitext(file)
-    new_filename = get_time_str() + filename + ext
-    
+    new_filename = get_time_str() + "-" + filename + ext
+
     return os.path.join(path, new_filename)
-    
 
 
 class MyMainForm(QWidget, Ui_Form):
@@ -49,23 +53,24 @@ class MyMainForm(QWidget, Ui_Form):
         super(MyMainForm, self).__init__(parent)
         self.setupUi(self)
 
-        self.img_blue = QPixmap('./src/drag-drop-blue.png')
-        self.img_grey = QPixmap('./src/drag-drop-grey.png')
+        self.img_blue = QPixmap(IMG_DRAG_DROP_BLUE_PATH)
+        self.img_grey = QPixmap(IMG_DRAG_DROP_GRAY_PATH)
 
-        self.setWindowTitle('公文格式化')
-        self.setWindowIcon(QIcon('./src/icon.png'))
+        self.setWindowTitle(STR_WINDOW_TITLE)
+        self.setWindowIcon(QIcon(IMG_ICON_PATH))
 
         x, y = self.getCenterPos()
 
         self.setGeometry(
             x, y, MAIN_FRAME_SIZE['WIDTH'], MAIN_FRAME_SIZE['HEIGHT'])
         self.setFixedSize(self.width(), self.height())
-        self.label_button.setPixmap(self.img_grey)
         self.label_button.setAlignment(Qt.AlignCenter)
-        self.progressBar.setValue(0)
         self.setAcceptDrops(True)
-        self.running = False
-        
+        self.eventRestore(None, None, None)
+
+    def setConfig(self, conf):
+        self.conf = conf
+
     def getCenterPos(self):
         '''
         获得居中坐标
@@ -76,7 +81,7 @@ class MyMainForm(QWidget, Ui_Form):
         y = int((screen.height() - MAIN_FRAME_SIZE['HEIGHT']) / 2)
 
         return x, y
-    
+
     def dragEnterEvent(self, event):
         '''
         拖入
@@ -95,86 +100,153 @@ class MyMainForm(QWidget, Ui_Form):
         移出
         '''
         self.label_button.setPixmap(self.img_grey)
-        
+
+    def showMessageBox(self, type, title, msg):
+        if type == "warning":
+            QMessageBox.warning(self, title, msg)
+
+
     def eventRestore(self, type, title, message):
         '''
         进度条重置
         '''
-        if type == "warning":
-            QMessageBox.warning(self, title, message)
-            
+        self.showMessageBox(type, title, message)
         self.label_button.setPixmap(self.img_grey)
         self.progressBar.setValue(0)
         self.label_progress.setText("(0/0)")
         self.running = False
+
+    def evenErrorCallback(self):
+        self.eventRestore("warning", STR_ERROR, STR_UNKNOWN)
+        
+    def evenLabelCallback(self, i, count):
+        self.label_progress.setText("({0}/{1})".format(i, count))
+        
+    def getAbility(self):
+        
+        ability = {'checkBox_b_title' : False, 
+                   'checkBox_a_title' : False, 
+                   'checkBox_target' : False, 
+                   'checkBox_tail' : False}
+        
+        if self.checkBox_b_title.isChecked():
+            ability['checkBox_b_title'] = True
+            
+        if self.checkBox_b_title.isChecked():
+            ability['checkBox_a_title'] = True
+            
+        if self.checkBox_target.isChecked():
+            ability['checkBox_target'] = True
+            
+        if self.checkBox_target.isChecked():
+            ability['checkBox_tail'] = True
+            
+        return ability
+            
 
     def dropEvent(self, event):
         '''
         松开鼠标
         '''
         if self.running:
-            self.eventRestore("warning", "错误", "有任务正在运行")
+            self.eventRestore("warning", STR_ERROR, STR_RUNNING_ERR)
             return
 
         file = event.mimeData().urls()[0].toLocalFile()
-        
+
         if not is_word_file(file):
-            self.eventRestore("warning", "错误", "文件类型错误，只支持doc/docx文件类型")
+            self.eventRestore("warning", STR_ERROR, STR_DOC_TYPE_ERR)
             return
-        
+
         self.running = True
-        
-        odf = ODFThread(file)
-        odf.run()
-        #odf.start()
-        
-
-
+        self.odf = ODFThread(file, self.conf, self.getAbility())
+        self.odf.sig_err.connect(self.evenErrorCallback)
+        self.odf.sig_label.connect(self.evenLabelCallback)
+        self.odf.run()
+        # odf.start()
 
 
 class ODFThread(QThread):
     '''
     image to pdf 线程
     '''
-    def __init__(self, file):
+    sig_err = pyqtSignal()
+    sig_label = pyqtSignal(int, int)
+ 
+    def __init__(self, file, conf, ability):
         '''
         构造函数
         '''
+
         super(ODFThread, self).__init__()
         self.file = file
+        self.conf = conf
+        self.ability = ability
+
+    def getStepCount(self):
+        c = 0
         
+        for a, s in self.ability.items():
+            if a == 'checkBox_b_title' and s:
+                c += 2
+                
+            if a == 'checkBox_a_title' and s:
+                c += 2
 
-
+        return 0
 
     def run(self):
         '''
         线程启动函数
         '''
+        self.getStepCount()
+
         try:
-            # dirs = getDirList(self.path)
-            # self.imgToPDF(dirs)
             if get_file_extension(self.file) in [".doc", ".DOC"]:
                 doc_to_docx(self.file)
-                               
-        except:
-            # self.triggerError.emit(True)
-            pass
-    
-def main():
-    '''
-    主函数
-    '''
 
+        except:
+            self.sig_err.emit()
+
+def load_conf(filename):
+    with codecs.open(filename, 'r', "utf-8") as f:
+        return json.load(f)
+
+
+def restore_conf(filename, data):
+    with codecs.open(filename, "w", "utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+    return data
+
+
+def get_conf(self):
+
+    data = None
+
+    try:
+        data = load_conf(CONF_FILENAME)
+    except:
+        QMessageBox.warning(self, STR_WARNING, STR_LOAD_CONF_ERR)
+        data = restore_conf(CONF_FILENAME, P_STYLE)
+
+    return data
+
+
+def main():
+    # '''
+    # 主函数
+    # '''
     app = QApplication(sys.argv)
     myWin = MyMainForm()
     myWin.show()
+    myWin.setConfig(get_conf(myWin))
 
     sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
     main()
-
 
 
 b_title_flag = True
@@ -185,23 +257,23 @@ target_line_flag = True
 tail_line_flag = True
 tail_line_count = 2
 
+
 class Para:
     text = ""
     content = ""
     type = ""
 
-    def __init__(self, text, type = None):
+    def __init__(self, text, type=None):
         if not type:
             self.type = self.get_type(text)
         else:
             self.type = type
-            
+
         if self.type in ('head2', 'head3', 'head4'):
             self.text, self.content = text.split("。", 1)
             self.text += "。"
         else:
             self.text = text
-            
 
     def get_type(self, text):
         for k, v in HEAD_SIGN.items():
@@ -240,10 +312,10 @@ class Doc:
         for line in self.doc:
             if len(line) > 0:
                 para.append(line)
-                #self.paragraphs.append(Para(line))
-        
+                # self.paragraphs.append(Para(line))
+
         return para
-    
+
     def load_para(self, para):
         b_title_c = 0
         title_done = False
@@ -252,53 +324,52 @@ class Doc:
         tail_line_c = 0
         tail_blank_line_done = False
         line_no = 0
-        
+
         for line in para:
             line_no += 1
-            
-            if b_title_flag and  b_title_c < b_title_line_count:
+
+            if b_title_flag and b_title_c < b_title_line_count:
                 self.paragraphs.append(Para(line, "b_title"))
                 b_title_c += 1
-                
+
                 if b_title_c == b_title_line_count:
                     self.paragraphs.append(Para("", "text"))
-                    
-                continue 
-                
+
+                continue
+
             if not title_done:
                 self.paragraphs.append(Para(line, "title"))
                 if not a_title_flag:
                     self.paragraphs.append(Para("", "text"))
 
                 title_done = True
-                continue 
-            
+                continue
+
             if a_title_flag and a_title_c < a_title_line_count:
                 self.paragraphs.append(Para(line, "a_title"))
                 a_title_c += 1
-                
+
                 if a_title_c == a_title_line_count:
                     self.paragraphs.append(Para("", "text"))
-                
+
                 continue
-                
+
             if target_line_flag and not target_line_done:
                 self.paragraphs.append(Para(line, "target"))
                 target_line_done = True
                 continue
-                
-            if tail_line_flag and line_no > len(para) - tail_line_count and tail_line_c < tail_line_count :
+
+            if tail_line_flag and line_no > len(para) - tail_line_count and tail_line_c < tail_line_count:
                 if not tail_blank_line_done:
-                    self.paragraphs.append(Para("", "text"))         
-                    self.paragraphs.append(Para("", "text"))    
+                    self.paragraphs.append(Para("", "text"))
+                    self.paragraphs.append(Para("", "text"))
                     tail_blank_line_done = True
-                    
+
                 self.paragraphs.append(Para(line, "tail"))
                 tail_line_c += 1
                 continue
-                
+
             self.paragraphs.append(Para(line))
-                
 
     def replace_x(self, text):
         t = text
@@ -321,162 +392,33 @@ class Doc:
     def write(self):
         doc = Document()
         for line in self.paragraphs:
-            
+
             p = doc.add_paragraph()
             run = p.add_run(line.text)
             run.font.name = 'Times New Roman'
-            run.element.rPr.rFonts.set(qn('w:eastAsia'), P_STYLE[line.type]['font'])
+            run.element.rPr.rFonts.set(
+                qn('w:eastAsia'), P_STYLE[line.type]['font'])
             run.font.size = Pt(FONT_SIZE_PT[P_STYLE[line.type]['font_size']])
-            
+
             if line.content:
                 run_content = p.add_run(line.content)
                 run_content.font.name = 'Times New Roman'
-                run_content.element.rPr.rFonts.set(qn('w:eastAsia'), P_STYLE['text']['font'])
-                run_content.font.size = Pt(FONT_SIZE_PT[P_STYLE['text']['font_size']])
+                run_content.element.rPr.rFonts.set(
+                    qn('w:eastAsia'), P_STYLE['text']['font'])
+                run_content.font.size = Pt(
+                    FONT_SIZE_PT[P_STYLE['text']['font_size']])
 
-            p.paragraph_format.space_before = Pt(P_STYLE[line.type]['space_before'])
-            p.paragraph_format.space_after = Pt(P_STYLE[line.type]['space_after'])
-            p.paragraph_format.line_spacing = Pt(P_STYLE[line.type]['line_spacing'])
+            p.paragraph_format.space_before = Pt(
+                P_STYLE[line.type]['space_before'])
+            p.paragraph_format.space_after = Pt(
+                P_STYLE[line.type]['space_after'])
+            p.paragraph_format.line_spacing = Pt(
+                P_STYLE[line.type]['line_spacing'])
             p.paragraph_format.alignment = PARA_ALIGN[P_STYLE[line.type]['align']]
-            p.paragraph_format.first_line_indent = run.font.size * P_STYLE[line.type]['indent']
+            p.paragraph_format.first_line_indent = run.font.size * \
+                P_STYLE[line.type]['indent']
 
         doc.save("t.docx")
-
-
-x_dict = {
-    ',': '，',
-    '.': '。',
-    '!': '！',
-    '(': '（',
-    ')': '）'
-}
-
-x_list = ['\t', ' ']
-
-head1 = ['一、', '二、', '三、', '四、', '五、',
-         '六、', '七、', '八、', '九、', '十、',
-         '十一、', '十二、', '十三、', '十四、', '十五、',
-         '十六、', '十七、', '十八、', '十九、', '二十、']
-
-head2 = ['（一）', '（二）', '（三）', '（四）', '（五）',
-         '（六）', '（七）', '（八）', '（九）', '（十）',
-         '（十一）', '（十二）', '（十三）', '（十四）', '（十五）',
-         '（十六）', '（十七）', '（十八）', '（十九）', '（二十）']
-
-head3 = ['1.', '2.', '3.', '4.', '5.',
-         '6.', '7.', '8.', '9.', '10.',
-         '11.', '12.', '13.', '14.', '15.',
-         '16.', '17.', '18.', '19.', '20.']
-
-head4 = ['（1）', '（2）', '（3）', '（4）', '（5）',
-         '（6）', '（7）', '（8）', '（9）', '（10）',
-         '（11）', '（12）', '（13）', '（14）', '（15）',
-         '（16）', '（17）', '（18）', '（19）', '（20）']
-
-HEAD_SIGN = {'head1': head1,
-             'head2': head2,
-             'head3': head3,
-             'head4': head4}
-
-FONT_SIZE_PT = {'二号': 22, '三号' : 16, '四号' : 14, '小四' : 12, '五号' : 10.5}
-PARA_ALIGN = {'居左' : WD_ALIGN_PARAGRAPH.LEFT, 
-              '居中' : WD_ALIGN_PARAGRAPH.CENTER,
-              '居右' : WD_ALIGN_PARAGRAPH.RIGHT,
-              '两端' : WD_ALIGN_PARAGRAPH.JUSTIFY}
-
-
-P_STYLE = {
-    'b_title': {
-        'font': '方正楷体_GBK',
-        'font_size': '三号',
-        'indent' : 0,
-        'align' : '两端',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    },
-    'title': {
-        'font': '方正小标宋_GBK',
-        'font_size': '二号',
-        'indent' : 0,
-        'align' : '居中',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    },
-    'a_title': {
-        'font': '方正楷体_GBK',
-        'font_size': '三号',
-        'indent' : 0,
-        'align' : '居中',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    },
-    'target': {
-        'font': '方正仿宋_GBK',
-        'font_size': '三号',
-        'indent' : 0,
-        'align' : '居左',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    },    
-    'head1': {
-        'font': '方正黑体_GBK',
-        'font_size': '三号',
-        'indent' : 2,
-        'align' : '两端',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    },
-    'head2': {
-        'font': '方正黑体_GBK',
-        'font_size': '三号',
-        'indent' : 2,
-        'align' : '两端',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    },
-    'head3': {
-        'font': '方正黑体_GBK',
-        'font_size': '三号',
-        'indent' : 2,
-        'align' : '两端',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    },
-    'head4': {
-        'font': '方正黑体_GBK',
-        'font_size': '三号',
-        'indent' : 2,
-        'align' : '两端',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    },
-    'tail': {
-        'font': '方正仿宋_GBK',
-        'font_size': '三号',
-        'indent' : 0,
-        'align' : '居右',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    },
-    'text': {
-        'font': '方正仿宋_GBK',
-        'font_size': '三号',
-        'indent' : 2,
-        'align' : '两端',
-        'space_before' : 0,
-        'space_after' : 0,
-        'line_spacing' : 28.8
-    }
-}
 
 
 # doc = Doc("test.docx")
---
+++ odf_conf.py
@@ -0,0 +1,153 @@
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+
+STR_WINDOW_TITLE = "公文格式化"
+STR_WARNING = "警告"
+STR_ERROR = "错误"
+STR_UNKNOWN = "未知错误"
+STR_LOAD_CONF_ERR = "未找到配置文件，已将配置文件恢复为默认值"
+STR_RUNNING_ERR = "有任务正在运行"
+STR_DOC_TYPE_ERR = "文件类型错误，只支持doc/docx文件类型"
+
+MAIN_FRAME_SIZE = {'WIDTH': 300, 'HEIGHT': 400}
+CONF_FILENAME = "conf.json"
+
+IMG_DRAG_DROP_BLUE_PATH = './src/drag-drop-blue.png'
+IMG_DRAG_DROP_GRAY_PATH = './src/drag-drop-grey.png'
+IMG_ICON_PATH = './src/icon.png'
+
+P_STYLE = {
+    'b_title': {
+        'font': '方正楷体_GBK',
+        'font_size': '三号',
+        'indent' : 0,
+        'align' : '两端',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    },
+    'title': {
+        'font': '方正小标宋_GBK',
+        'font_size': '二号',
+        'indent' : 0,
+        'align' : '居中',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    },
+    'a_title': {
+        'font': '方正楷体_GBK',
+        'font_size': '三号',
+        'indent' : 0,
+        'align' : '居中',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    },
+    'target': {
+        'font': '方正仿宋_GBK',
+        'font_size': '三号',
+        'indent' : 0,
+        'align' : '居左',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    },    
+    'head1': {
+        'font': '方正黑体_GBK',
+        'font_size': '三号',
+        'indent' : 2,
+        'align' : '两端',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    },
+    'head2': {
+        'font': '方正黑体_GBK',
+        'font_size': '三号',
+        'indent' : 2,
+        'align' : '两端',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    },
+    'head3': {
+        'font': '方正黑体_GBK',
+        'font_size': '三号',
+        'indent' : 2,
+        'align' : '两端',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    },
+    'head4': {
+        'font': '方正黑体_GBK',
+        'font_size': '三号',
+        'indent' : 2,
+        'align' : '两端',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    },
+    'tail': {
+        'font': '方正仿宋_GBK',
+        'font_size': '三号',
+        'indent' : 0,
+        'align' : '居右',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    },
+    'text': {
+        'font': '方正仿宋_GBK',
+        'font_size': '三号',
+        'indent' : 2,
+        'align' : '两端',
+        'space_before' : 0,
+        'space_after' : 0,
+        'line_spacing' : 28.8
+    }
+}
+
+
+
+x_dict = {
+    ',': '，',
+    '.': '。',
+    '!': '！',
+    '(': '（',
+    ')': '）'
+}
+
+x_list = ['\t', ' ']
+
+head1 = ['一、', '二、', '三、', '四、', '五、',
+         '六、', '七、', '八、', '九、', '十、',
+         '十一、', '十二、', '十三、', '十四、', '十五、',
+         '十六、', '十七、', '十八、', '十九、', '二十、']
+
+head2 = ['（一）', '（二）', '（三）', '（四）', '（五）',
+         '（六）', '（七）', '（八）', '（九）', '（十）',
+         '（十一）', '（十二）', '（十三）', '（十四）', '（十五）',
+         '（十六）', '（十七）', '（十八）', '（十九）', '（二十）']
+
+head3 = ['1.', '2.', '3.', '4.', '5.',
+         '6.', '7.', '8.', '9.', '10.',
+         '11.', '12.', '13.', '14.', '15.',
+         '16.', '17.', '18.', '19.', '20.']
+
+head4 = ['（1）', '（2）', '（3）', '（4）', '（5）',
+         '（6）', '（7）', '（8）', '（9）', '（10）',
+         '（11）', '（12）', '（13）', '（14）', '（15）',
+         '（16）', '（17）', '（18）', '（19）', '（20）']
+
+HEAD_SIGN = {'head1': head1,
+             'head2': head2,
+             'head3': head3,
+             'head4': head4}
+
+FONT_SIZE_PT = {'二号': 22, '三号': 16, '四号': 14, '小四': 12, '五号': 10.5}
+PARA_ALIGN = {'居左': WD_ALIGN_PARAGRAPH.LEFT,
+              '居中': WD_ALIGN_PARAGRAPH.CENTER,
+              '居右': WD_ALIGN_PARAGRAPH.RIGHT,
+              '两端': WD_ALIGN_PARAGRAPH.JUSTIFY}
